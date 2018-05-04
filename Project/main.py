d264c0c46da78385ce2a35232734e02b610a2fe2
pmorvalho <pmorvalho@gmail.com>
2018-05-04 12:24:12 +0100

    different colors for different companies, list of colors

--- Project/main.py
+++ Project/main.py
@@ -5,6 +5,7 @@ from company import *
 from truck import *
 from item import *
      
+colors = ['#8B0000','#8FBC8F','#00BFFF','#B22222','#FF69B4','#90EE90','#87CEFA', '#00FF00','#000080','#FF6347','#9ACD32']#,'#7FFF00','#FF7F50','#6495ED','#FFF8DC','#DC143C','#00FFFF','#00008B','#008B8B','#B8860B','#A9A9A9','#006400','#BDB76B','#8B008B']
 
 def set_company_nodes(graph):
     company_names = ["A", "B", "C", "D","E"]
@@ -17,13 +18,17 @@ def set_company_nodes(graph):
     return graph
 
 
-def randColor():
-    return "#%06x" % randint(0x111111,0xCCCCCC)
+def randColor(colormap):
+    color = colors[randint(0,len(colors)-1)]
+    while color in colormap:
+        color = colors[randint(0,len(colors)-1)]
+    return color
 
 def draw_graph(graph):
     colormap = []
+
     for n in graph.nodes:
-        colormap.append(randColor()) if "company" in graph.node[n] else colormap.append("#%06x" % 0xDDDDDD)
+        colormap.append(randColor(colormap)) if "company" in graph.node[n] else colormap.append("#%06x" % 0xDDDDDD)
             
     d = dict(nx.degree(graph))
     pos = nx.spring_layout(graph) 
@@ -41,8 +46,7 @@ for (u,v,w) in g.edges(data=True):
     w['weight'] = randint(1,10)
 g = set_company_nodes(g)
 
-
-# draw_graph(g)
+draw_graph(g)
 
 companies = [g.nodes[n]["company"] for n in g.nodes if "company" in g.node[n]]
 clients = [n for n in g.nodes if "company" not in g.node[n]]
