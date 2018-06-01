177b18031ff63da4c461ede53dc6edf17f465625
pmorvalho <pmorvalho@gmail.com>
2018-06-01 12:14:33 +0100

    now catching RunTimeWarning

--- client.py
+++ client.py
@@ -5,6 +5,7 @@ import math
 import numpy as np
 from random import *
 import copy
+import warnings
 
 class Client:
 	def __init__(self, pos, companies, risk=(randint(1,99)/100), utilities=[], min_offer_val=20, max_offer_val=100):
@@ -28,7 +29,12 @@ class Client:
 		return Offer(self.pos, randint(self.min_offer_val, self.max_offer_val), i) if (randint(1,99)/100) < self.risk else None
 
 	def chooseBestBid(self, bids):
-		bids_values = [(1-self.utilities[i])*bids[i] for i in range(len(bids))]
+		with warnings.catch_warnings():
+			warnings.filterwarnings('error')
+			try:
+				bids_values = [(1-self.utilities[i])*bids[i] for i in range(len(bids))]
+			except Warning as e:
+				return None
 		return bids_values.index(min(bids_values))
 
 	def removeCompany(self, company):
@@ -42,6 +48,8 @@ class Client:
 		if offer != None:
 			offers = [c.getBid(offer) for c in self.companies]
 			company = self.chooseBestBid(offers)
+			if company == None:
+				return
 			# print("Client ", self.pos, " requesting offer from ", self.companies[company].pos, "  ",offers[company].getValue(),"  ", self.companies)
 			if offers[company] != math.inf:
 				offer.setValue(offers[company])
